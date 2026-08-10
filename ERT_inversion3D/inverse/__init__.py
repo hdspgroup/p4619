66c2d3f5bed22158cc53450820a01c641402dcfe
SantiagoDelgado11 <santiago2192691@correo.uis.edu.co>
2026-08-10 08:46:35 -0500

    Add ERT inversion 3D

--- ERT_inversion3D/inverse/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import PotentialNet, ConductivityNet
-from .physics_informer import PhysicsInformer
-
-__all__ = ["PotentialNet", "ConductivityNet", "PhysicsInformer"]
--- ERT_inversion3D/inverse/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .potential_net import PotentialNet
-from .conductivity_net import ConductivityNet
-from .fourier_features import FourierFeatureMapping
-from .mlp import MLP
-
-__all__ = ["PotentialNet", "ConductivityNet", "FourierFeatureMapping", "MLP"]
